2cb8a12b982447c6f9c5d963743cc512da400b6e
John McNeil <johnmcneil@users.noreply.github.com>
2020-09-04 15:56:29 -0500

    Update musical-robot.py

--- musical-robot.py
+++ musical-robot.py
@@ -1,2 +1,23 @@
-"https://www.classicalmpr.org/topic/daily-download"
-"http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/09/03/daily_download_20200903_128.mp3"
+# "https://www.classicalmpr.org/topic/daily-download"
+# "http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/09/03/daily_download_20200903_128.mp3"
+
+# https://stackabuse.com/using-curl-in-python-with-pycurl/
+
+import pycurl
+from io import BytesIO
+
+b_obj = BytesIO()
+crl = pycurl.Curl()
+
+# url
+crl.setopt(crl.URL, 'http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/09/03/daily_download_20200903_128.mp3')
+
+crl.setopt(crl.WRITEDATA, b_obj)
+
+crl.perform()
+
+crl.close()
+
+get_body = b_obj.getvalue()
+
+
