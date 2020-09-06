a68c0eaacb24cf7094c575d9198233545c4a473d
John McNeil <johnmcneil@users.noreply.github.com>
2020-09-06 18:26:56 -0500

    Update musical-robot.py

--- musical-robot.py
+++ musical-robot.py
@@ -1,10 +1,12 @@
-# "https://www.classicalmpr.org/topic/daily-download"
-# "http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/09/03/daily_download_20200903_128.mp3"
+import pycurl
+from io import BytesIO
+
+baseURL = "https://www.classicalmpr.org/topic/daily-download"
+fullURL = "http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/09/03/daily_download_20200903_128.mp3"
 
 # https://stackabuse.com/using-curl-in-python-with-pycurl/
 
-import pycurl
-from io import BytesIO
+
 
 b_obj = BytesIO()
 crl = pycurl.Curl()
