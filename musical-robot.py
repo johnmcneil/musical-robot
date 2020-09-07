0ea71dcb1e30ecb1e5ee8f750e3652c81137a599
John McNeil <johnmcneil@users.noreply.github.com>
2020-09-07 12:03:38 -0500

    Update musical-robot.py

--- musical-robot.py
+++ musical-robot.py
@@ -1,8 +1,10 @@
 import pycurl
 from io import BytesIO
+import certifi
 
-baseURL = "https://www.classicalmpr.org/topic/daily-download"
-fullURL = "http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/09/03/daily_download_20200903_128.mp3"
+
+baseURL = 'https://www.classicalmpr.org/topic/daily-download'
+fullURL = 'http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/09/07/daily_download_20200907_128.mp3'
 destinationFile = "robot-music.mp3", "wb"
 # https://stackabuse.com/using-curl-in-python-with-pycurl/
 
@@ -10,13 +12,19 @@ destinationFile = "robot-music.mp3", "wb"
 b_obj = BytesIO()
 crl = pycurl.Curl()
 
+crl.setopt(crl.CAINFO, certifi.where())
 # url
-crl.setopt(crl.URL, 'fullURL')
+crl.setopt(crl.URL, baseURL)
 
-crl.setopt(crl.WRITEDATA, 'destinationFile')
 
+
+crl.setopt(crl.WRITEDATA, b_obj)
 
 crl.perform()
 
 crl.close()
 
+
+get_body = b_obj.getvalue()
+
+print('Output of GET request:\n%s' % get_body.decode('utf8')) 
