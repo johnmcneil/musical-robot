4ad28ea0a001c2144dbfc67ecb2ab14a46a16879
John McNeil <johnmcneil@users.noreply.github.com>
2020-09-07 12:11:39 -0500

    tuning up the musical robot

--- musical-robot.py
+++ musical-robot.py
@@ -1,3 +1,5 @@
+# https://stackabuse.com/using-curl-in-python-with-pycurl/
+
 import pycurl
 from io import BytesIO
 import certifi
@@ -6,22 +8,18 @@ import certifi
 baseURL = 'https://www.classicalmpr.org/topic/daily-download'
 fullURL = 'http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/09/07/daily_download_20200907_128.mp3'
 destinationFile = "robot-music.mp3", "wb"
-# https://stackabuse.com/using-curl-in-python-with-pycurl/
-
+file = open('pycurl.md', 'wb')
 
 b_obj = BytesIO()
 crl = pycurl.Curl()
 
 crl.setopt(crl.CAINFO, certifi.where())
+
+
 # url
-crl.setopt(crl.URL, baseURL)
-
-
-
-crl.setopt(crl.WRITEDATA, b_obj)
-
+crl.setopt(crl.URL, fullURL)
+crl.setopt(crl.WRITEDATA, file)
 crl.perform()
-
 crl.close()
 
 
