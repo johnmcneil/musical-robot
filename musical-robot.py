46504d621651918964c2e221925a8560108dca87
John McNeil <johnmcneil@users.noreply.github.com>
2020-09-08 19:51:50 -0500

    Automated putting today's date in the string for the url for today's mp3.

--- musical-robot.py
+++ musical-robot.py
@@ -7,20 +7,21 @@ from io import BytesIO
 import certifi
 import datetime
 
-
-baseURL = 'https://www.classicalmpr.org/topic/daily-download'
-fullURL = 'http://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/2020/08/13/daily_download_20200813_128.mp3'
-
 destination_filename_base = 'daily_download_'
 
 date = datetime.datetime.now()
 date_str = '{:%Y%m%d}'.format(date)
+date_str_url = '{:%Y/%m/%d/}'.format(date)
 
-destination_filename = 'scratch_' + destination_filename_base + date_str + '_128.mp3'
-path = 'G:\Music\Classical MPR Daily Download\\' + destination_filename
+destination_filename = destination_filename_base + date_str + '_128.mp3'
+path = 'G:\Music\Classical MPR Daily Download\\' + destination_filename + '_scratch'
+
+baseURL = 'https://www.classicalmpr.org/topic/daily-download'
+fullURL = 'https://download.publicradio.org/podcast/minnesota/classical/programs/free-downloads/' + date_str_url + destination_filename
+
+print(fullURL)
 
 print(path)
-
 destination_file = open(path, 'wb')
 
 
@@ -34,5 +35,4 @@ crl.setopt(crl.URL, fullURL)
 crl.setopt(crl.WRITEDATA, destination_file)
 crl.perform()
 crl.close()
-
 
